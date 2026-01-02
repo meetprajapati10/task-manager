94a736dce09a32762ac764a1fead1bb0f572d88f
Meet Prajapati <meetprajapati5243@gmail.com>
2026-01-03 00:13:35 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
