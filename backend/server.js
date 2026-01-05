6dff0425b17e6bf3b780781573c6614fa93f7eda
Meet Prajapati <meetprajapati5243@gmail.com>
2026-01-05 23:50:34 +0530

    implement database connection and define task and user models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,28 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from "cors";
+import path from "path";
+import connectDB from "./config/db.js";
 
 const app = express();
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
 
 // Start Server
 const PORT = process.env.PORT || 5000;
