9175d5522708d7e54e7600e9e0914ace49399d4f
Meet Prajapati <meetprajapati5243@gmail.com>
2026-01-07 23:55:28 +0530

    feat: implement routing and create admin/user pages for task management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,46 @@
-import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+
+import PrivateRoute from "./routes/PrivateRoute";
+import Dashboard from "./pages/Admin/Dashboard";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import CreateTask from "./pages/Admin/CreateTask";
+import ManageUsers from "./pages/Admin/ManageUsers";
+
+import UserDashboard from "./pages/User/Dashboard";
+import MyTasks from "./pages/User/MyTasks";
+import ViewTaskDetails from "./pages/User/ViewTaskDetails";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="text-5xl font-semibold">Task Manager</h1>
-      <Button>Click me</Button>
+    <div>
+      <Router>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+            <Route path="/admin/users" element={<ManageUsers />} />
+          </Route>
+
+          {/* User Routes */}
+          <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route
+              path="/user/task-details/:id"
+              element={<ViewTaskDetails />}
+            />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
